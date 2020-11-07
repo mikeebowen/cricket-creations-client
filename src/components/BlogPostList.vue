ca647e8569c4fa0579cbfbaa26cc9f2c73134b73
Michael Bowen <mikee.bowen@gmail.com>
2020-11-07 13:42:32 -0800

    add BlogPostList and login form

---
+++ src/components/BlogPostList.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card max-width="600" class="mx-auto">
+    <v-toolbar color="light-blue" dark>
+      <v-toolbar-title>My Posts</v-toolbar-title>
+      <v-list subheader two-line>
+        <v-list-item v-for="post in posts" :key="post.id">
+          <v-list-item-avatar>
+            <v-icon class="blue" dark v-text="'mdi-clipboard-text'" />
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title v-text="post.title" />
+
+            <v-list-item-subtitle v-text="post.lastUpdated" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'BlogPostList',
+  props: {
+    posts: { type: Array, default: () => [] },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,13 +1,21 @@
 <template>
-  <div />
+  <v-container>
+    <BlogPostList :posts="articles" />
+  </v-container>
 </template>
 
 <script>
+import BlogPostList from '@/components/BlogPostList'
 export default {
   name: 'Admin',
+  components: { BlogPostList },
+  computed: {
+    articles() {
+      return []
+    },
+  },
+  mounted() {},
 }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
