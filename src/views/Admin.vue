7eb9df121e2d2732e107ddd8c69088e645681ac2
Michael Bowen <mikee.bowen@gmail.com>
2020-12-18 10:36:25 -0800

    add locally hosted tinymce because I don't want to do the stupid tiny cloud boo

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,5 +1,8 @@
 <template>
-  <v-container class="post-list d-flex flex-column">
+  <v-container v-if="selectedPost">
+    <Editor :init="editorConfig" />
+  </v-container>
+  <v-container v-else class="post-list d-flex flex-column">
     <v-row>
       <v-col offset="2" cols="10">
         <BlogPostList :posts="posts" @postSelected="selectPost" />
@@ -11,11 +14,23 @@
 
 <script>
 import BlogPostList from '@/components/BlogPostList'
+import Editor from '@tinymce/tinymce-vue'
+import 'tinymce/themes/silver'
+import 'tinymce/plugins/table'
+import 'tinymce/plugins/link'
+import 'tinymce/plugins/image'
+import 'tinymce/plugins/spellchecker'
+import 'tinymce/plugins/imagetools'
+import 'tinymce/plugins/save'
+import 'tinymce/plugins/lists'
+import 'tinymce/plugins/codesample'
+import 'tinymce/plugins/code'
 import axios from 'axios'
+import { resolve } from 'path'
 import { ref, onMounted, watch, computed } from '@vue/composition-api'
 export default {
   name: 'Admin',
-  components: { BlogPostList },
+  components: { BlogPostList, Editor },
   setup() {
     const page = ref(1)
     const count = ref(10)
@@ -29,18 +44,46 @@ export default {
       t.value = ps?.data?.meta?.total
       posts.value.push(...ps?.data?.data)
     }
+    const selectedPost = ref(null)
     const selectPost = post => {
       console.log('🚀 ~ file: Admin.vue ~ line 34 ~ constselectPost ~ post', post)
+      console.log(resolve(__dirname, __filename))
+      selectedPost.value = post
     }
 
-    watch(page, (cur, prev) => {
-      getBlogPosts()
+    const editorConfig = ref({
+      height: 500,
+      // menubar: false,
+      plugins: ['link', 'table', 'spellchecker', 'image', 'imagetools', 'save', 'lists', 'imagetools', 'codesample', 'code '],
+      // tslint:disable-next-line:max-line-length
+      toolbar:
+        'insertfile undo redo | code | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image codesample | save cancel',
+      menubar: 'file edit insert view format table tools image',
+      browser_spellcheck: true,
+      codesample_languages: [
+        { text: 'HTML/XML', value: 'markup' },
+        { text: 'JavaScript', value: 'javascript' },
+        { text: 'CSS', value: 'css' },
+        { text: 'PHP', value: 'php' },
+        { text: 'Ruby', value: 'ruby' },
+        { text: 'Python', value: 'python' },
+        { text: 'Java', value: 'java' },
+        { text: 'C', value: 'c' },
+        { text: 'C#', value: 'csharp' },
+        { text: 'C++', value: 'cpp' },
+      ],
+    })
+    const editor = ref(null)
+
+    watch(page, async (cur, prev) => {
+      await getBlogPosts()
     })
 
-    onMounted(() => {
-      getBlogPosts()
+    onMounted(async () => {
+      console.log('editor: ', editor.value)
+      await getBlogPosts()
     })
-    return { page, count, posts, total, error, selectPost }
+    return { page, count, posts, total, error, selectPost, editorConfig, selectedPost, editor }
   },
 }
 </script>
