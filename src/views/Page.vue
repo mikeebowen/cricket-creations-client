7d91d45829b2c009455b4a2781af3061d4bba8f8
Michael Bowen <mikee.bowen@gmail.com>
2021-11-21 10:37:32 -0800

    style pages

--- src/views/Page.vue
+++ src/views/Page.vue
@@ -3,11 +3,45 @@
     <v-row class="flex-grow-0">
       <v-col>
         <h1 class="text-center">{{ page.title }}</h1>
+        <div class="decorative-line">
+          <div class="decorative-line-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16.395" height="20.284" viewBox="0 0 16.395 20.284">
+              <g id="Group_197" data-name="Group 197" transform="translate(-689.803 -586.663)">
+                <rect
+                  id="Rectangle_362"
+                  data-name="Rectangle 362"
+                  width="7"
+                  height="13"
+                  transform="translate(692.403 591.991) rotate(-21)"
+                  fill="#fff"
+                />
+                <rect
+                  id="Rectangle_198"
+                  data-name="Rectangle 198"
+                  width="0.999"
+                  height="19.965"
+                  transform="translate(690.449 587.668) rotate(-21.05)"
+                  stroke="#000"
+                  stroke-width="1"
+                />
+                <rect
+                  id="Rectangle_199"
+                  data-name="Rectangle 199"
+                  width="0.999"
+                  height="19.965"
+                  transform="translate(697.448 587.668) rotate(-21.05)"
+                  stroke="#000"
+                  stroke-width="1"
+                />
+              </g>
+            </svg>
+          </div>
+        </div>
       </v-col>
     </v-row>
     <v-row>
       <v-col class="d-flex flex-column">
-        <v-sheet elevation="2" class="flex-grow-1 pa-12" v-html="page.content" />
+        <v-sheet elevation="2" class="flex-grow-1 px-12 pt-6" v-html="page.content" />
       </v-col>
     </v-row>
   </v-container>
@@ -53,5 +87,21 @@ export default {
 
 .container {
   max-width: 1000px;
+}
+
+.decorative-line {
+  border-top: 2px solid #000;
+  height: 13px;
+  margin-top: 8px;
+  margin-bottom: 25px;
+  text-align: center;
+  max-width: 1000px;
+  margin-left: auto;
+  margin-right: auto;
+
+  .decorative-line-center {
+    position: relative;
+    top: -18px;
+  }
 }
 </style>
