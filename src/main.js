8fc6ee32365d4c577a98b1e47beaca631624b75b
Michael Bowen <mikee.bowen@gmail.com>
2022-03-05 13:02:36 -0800

    add google analytics

--- src/main.js
+++ src/main.js
@@ -6,13 +6,22 @@ import router from './router'
 import store from './store/store'
 import vuetify from './plugins/vuetify'
 import VueCompositionAPI from '@vue/composition-api'
+import VueGtag from 'vue-gtag'
 import '@fortawesome/fontawesome-free/css/all.css'
-import VueAnalytics from 'vue-analytics'
 
-Vue.use(VueAnalytics, {
-  id: process.env.VUE_APP_ANALYTICS_KEY,
+Vue.use(
+  VueGtag,
+  {
+    config: { id: process.env?.GOOGLE_ANALYTICS_KEY },
+    pageTrackerTemplate(to) {
+      return {
+        page_title: to.name,
+        page_path: to.path,
+      }
+    },
+  },
   router,
-})
+)
 Vue.use(Vuelidate)
 Vue.config.productionTip = false
 Vue.use(VueMasonry)
